a067543bf90565eb686d5d8e77f9e53a6688dd6f
coreymb99 <62529226+coreymb99@users.noreply.github.com>
2020-04-03 23:52:27 -0400

    Initial commit

---
+++ app/src/main/java/com/example/yourschoolapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.yourschoolapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Instantiate the Button class with the Button widget
+        Button btnInfo = (Button) findViewById(R.id.btnInfo);
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this, Information.class));
+
+            } //end onClick
+        }); //end btnInfo
+
+
+
+    } // end onCreate
+} // end MainActivity
